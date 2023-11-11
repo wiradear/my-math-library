cf204b06cdb9e6c88982cb5decedba9d3aa9f9c6
wiradear <wirakarn.pon@dome.tu.ac.th>
2023-11-11 13:17:11 +0700

    Add sum array functionality reduce

--- main.js
+++ main.js
@@ -8,12 +8,9 @@
 
   function sum(a, b) {
     if (Array.isArray(a)) {
-    let sum = 0
-    a.forEach((i) => (sum += i))
-    return sum
-      } else if (typeof a === 'number' && typeof b === 'number') {
-        return a + b
-    }
+    return a.reduce((acc, n) => acc + n, 0)
+    } else if (typeof a === 'number' && typeof b === 'number') {
+    return a + b }
     }
     module.exports = {
         sum,
