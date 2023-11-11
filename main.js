a63b8a588ed54db6b06858800340d5bedc3634ab
wiradear <wirakarn.pon@dome.tu.ac.th>
2023-11-11 11:22:49 +0700

    Initail the math library

---
+++ main.js
@@ -0,0 +1,7 @@
+  // 📄 math.js
+  function sum(a, b) { 
+    return a + b
+  }
+  module.exports = {
+     sum,
+  }
---
+++ main.test.js
@@ -0,0 +1,5 @@
+// 📄
+const math = require('./main')
+const assert = require('assert')
+
+assert.strictEqual(math.sum(2, 2), 4, 'should sum two numbers')
